f2a58efd6e00b4a52553ba1689aa275eee0b7cc0
Bhavya Jain <bhavya5jain@gmail.com>
2024-08-05 03:31:20 +0900

    Updated config

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 
 	"github.com/jaingounchained/todo/api"
 	db "github.com/jaingounchained/todo/db/sqlc"
-	storage "github.com/jaingounchained/todo/storage/local_directory"
+	storage "github.com/jaingounchained/todo/storage"
+	localStorage "github.com/jaingounchained/todo/storage/local_directory"
 	"github.com/jaingounchained/todo/util"
 	_ "github.com/lib/pq"
 )
@@ -32,16 +33,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	localStoragePath := filepath.Join(cwd, "uploads")
-	localStorage, err := storage.New(localStoragePath)
-	if err != nil {
-		log.Fatal("cannot setup storage:", err)
+	var storage storage.Storage
+	if config.StorageType == "LOCAL" {
+		localStoragePath := filepath.Join(cwd, config.LocalStorageDirectory)
+		storage, err = localStorage.New(localStoragePath)
+		if err != nil {
+			log.Fatal("cannot setup storage:", err)
+		}
 	}
 
 	store := db.NewStore(conn)
 
 	// TODO: configure server to not limit the number of incoming bytes to ~10 MB: Give max memory to the gin engine
-	server := api.NewServer(store, localStorage)
+	server := api.NewServer(store, storage)
 
 	err = server.Start(config.ServerAddress)
 	if err != nil {
--- util/config.go
+++ util/config.go
@@ -5,9 +5,11 @@ import (
 )
 
 type Config struct {
-	DBDriver      string `mapstructure:"DB_DRIVER"`
-	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	DBDriver              string `mapstructure:"DB_DRIVER"`
+	DBSource              string `mapstructure:"DB_SOURCE"`
+	ServerAddress         string `mapstructure:"SERVER_ADDRESS"`
+	StorageType           string `mapstructure:"STORAGE_TYPE"`
+	LocalStorageDirectory string `mapstructure:"LOCAL_STORAGE_DIRECTORY"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
