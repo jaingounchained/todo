18132d48ff0ec830cfd51ba3b7077020d89976aa
Bhavya Jain <bhavya5jain@gmail.com>
2024-08-15 11:02:58 +0900

    Added password creation utility and some other helper methods

---
+++ util/password.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	PasswordCreateHashError = errors.New("Couldn't hash the provided password")
+)
+
+// HashPassword returns the bcrypt hash of the password
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", PasswordCreateHashError
+	}
+	return string(hashedPassword), nil
+}
+
+// CheckPassword checks if the provided password is correct or not
+func CheckPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -42,4 +43,12 @@ func RandomString(n int) string {
 func RandomStringPointer(n int) *string {
 	s := RandomString(n)
 	return &s
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
