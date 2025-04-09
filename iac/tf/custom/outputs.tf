1337705cd47207b883b6cb1e8d142f9143d12573
stackgen-automation <automation@stackgen.com>
2025-04-09 18:10:10 +0000

    Automated commit 2

---
+++ iac/tf/custom/outputs.tf
@@ -0,0 +1,20 @@
+
+output "aws_s3_stackgen_bfbbd994-cad9-4b1d-ba36-47bc7a89d6bc_arn" {
+  value = module.stackgen_bfbbd994-cad9-4b1d-ba36-47bc7a89d6bc.arn
+  sensitive = false
+}
+
+output "aws_s3_stackgen_bfbbd994-cad9-4b1d-ba36-47bc7a89d6bc_bucket_name" {
+  value = module.stackgen_bfbbd994-cad9-4b1d-ba36-47bc7a89d6bc.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_bfbbd994-cad9-4b1d-ba36-47bc7a89d6bc_bucket_website_endpoint" {
+  value = module.stackgen_bfbbd994-cad9-4b1d-ba36-47bc7a89d6bc.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_stackgen_bfbbd994-cad9-4b1d-ba36-47bc7a89d6bc_kms_arn" {
+  value = module.stackgen_bfbbd994-cad9-4b1d-ba36-47bc7a89d6bc.kms_arn
+  sensitive = false
+}
